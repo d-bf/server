be526b5a5134e6abb6c9249a0936f1ff65f52840
RezaSR <rsr_reza@yahoo.com>
2015-11-08 07:13:41 +0330

    Prepare main query for selecting a task

--- www/modules/api/common/controllers/TaskController.php
+++ www/modules/api/common/controllers/TaskController.php
@@ -24,14 +24,11 @@ class TaskController extends Controller
         if (!empty($reqData['client_info']) && !empty($reqData['client_info']['platform'])) {
             $task = [];
             foreach ($reqData['client_info']['platform'] as $platform) {
-                $getTaskInfo['pu'] = '';
+                $getTaskInfo['platform'] = '';
                 $getTaskInfo['benchmark'] = '';
-                // os_arch_pu_brand
-                if (!empty($platform['id'])) {
-                    $platformInfo = explode('_', $platform['id']); // $platformInfo indexes are: 0: os, 1: arch, 2: pu (processing uint), 3: brand
-                    if (!empty($platformInfo[2]))
-                        $getTaskInfo['pu'] = $platformInfo[2];
-                }
+                
+                if (!empty($platform['id']))
+                    $getTaskInfo['platform'] = $platform['id'];
                 
                 if (!empty($platform['benchmark']))
                     $getTaskInfo['benchmark'] = $platform['benchmark'];
@@ -50,16 +47,47 @@ class TaskController extends Controller
 
     /**
      * Retrieve a suitable task from database
-     * 
      * @param array $info
      */
     protected function getTask($info)
     {
+        if (explode('_', $info['platform'])[2] == 'gpu')
+            $rateSelector = 'rate_gpu';
+        else
+            $rateSelector = 'rate_cpu';
+        
+        $task = \Yii::$app->db->createCommand("SELECT c.id AS id, g.name AS gen_name, a.id AS algo_id, a.name AS algo_name, a.$rateSelector AS algo_rate, c.len_min AS lenMin, c.len_max AS lenMax, c.charset_1 AS charset1, c.charset_2 AS charset2, c.charset_3 AS charset3, c.charset_4 AS charset4, c.mask AS mask, c.key_total AS keyTotal, c.key_assigned AS keyAssigned, c.res_assigned AS resAssigned FROM {{%crack}} c JOIN {{%generator}} g ON (c.id = :crackId AND g.id = c.gen_id) JOIN {{%algorithm}} a ON a.id = c.algo_id", [
+            ':crackId' => \Yii::$app->db->createCommand("SELECT c.id FROM {{%crack}} c JOIN {{%crack_platform}} cp ON (cp.platform_name = :platformName AND c.id = cp.crack_id) ORDER BY c.res_assigned ASC, c.key_total DESC, c.key_assigned ASC LIMIT 1", [
+                ':platformName' => $info['platform']
+            ])->queryScalar()
+        ])->queryOne();
+        
+        // Calculate speed of current algorithm
+        // TODO: Use the client's benchmark of current algorithm if available 
+        if ($task['algo_rate'] === null)
+            $rate = 0.5;
+        else
+            $rate = $task['algo_rate'];
+        $power = $info['benchmark'] * rate;
+        
+        // TODO: Assign dynamic amount of work based on remained keys.
+        // Assign 3 minute of work (3 min = 10800 sec)
+        $power *= 10800;
+        
+        $canAssign = $task['keyTotal'] - $task['keyAssigned'];
+        
+        if ($canAssign > $power) { // Task won't be finished by this assignment
+            $canAssign = $power;
+        }
+        
+        \Yii::$app->db->createCommand("UPDATE {{%crack}} SET key_assigned = key_assigned + :keyAssigned WHERE id = :crackId", [
+            ':keyAssigned' => $canAssign,
+            ':crackId' => $task['id']
+        ])->execute();
     }
 
     /**
      * Retrieve average speed of the system
-     * 
      * @param mixed $speed current client's speed
      */
     protected function getAvgSpeed($speed)
