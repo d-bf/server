bc55bb6c04bb7def292f1e94f0a0fcce5cfb25ff
RezaSR <rsr_reza@yahoo.com>
2016-02-15 20:02:14 +0330

    Stash get vendor info

--- www/modules/api/common/controllers/VendorController.php
+++ www/modules/api/common/controllers/VendorController.php
@@ -3,19 +3,19 @@ namespace app\modules\api\common\controllers;
 
 use app\modules\api\common\controllers;
 use app\modules\api\common\components\ApiComp;
+use yii\helpers\Json;
 
 class VendorController extends Controller
 {
 
     /**
-     * Send vendor file or inforamtion to client
+     * Send vendor file to client
      * request data:
      * {
      * "vendor_type" : "",
      * "name" : "",
      * "platform_id" : ""
      * }
-     * object_type: file | info
      * vendor_type: cracker | generator
      */
     public function actionGet()
@@ -30,6 +30,59 @@ class VendorController extends Controller
         }
         
         return 0;
+    }
+
+    /**
+     * Send vendor inforamtion to client
+     * request data:
+     * {
+     * "vendor_type" : "",
+     * "name" : "",
+     * "platform_id" : ""
+     * }
+     * vendor_type: cracker | generator
+     */
+    public function actionGetinfo()
+    {
+        $reqData = \Yii::$app->request->post();
+        
+        $info = '';
+        
+        if (isset($reqData['vendor_type']) && isset($reqData['name']) && isset($reqData['platform_id'])) {
+            if ($reqData['vendor_type'] == 'cracker') { // Cracker's information
+                $crackerInfo = \Yii::$app->db->createCommand("SELECT c.id AS id, c.config AS config FROM {{%cracker}} c WHERE c.name = :crackerName", [
+                    ':crackerName' => $reqData['name']
+                ])->queryOne();
+                
+                if ($crackerInfo) {
+                    $info = Json::decode($crackerInfo['config'], true);
+                    
+                    $info['generator'] = [];
+                    $crackerGeneratorsInfo = \Yii::$app->db->createCommand("SELECT g.name AS name, cg.config AS config FROM {{%cracker_gen}} cg JOIN {{%generator}} g ON (cg.cracker_id = :crackerId AND g.id = cg.gen_id)", [
+                        ':crackerId' => $crackerInfo['id']
+                    ])->queryAll();
+                    if ($crackerGeneratorsInfo) {
+                        foreach ($crackerGeneratorsInfo as $crackerGeneratorInfo) {
+                            array_push($info['generator'], [
+                                'name' => $crackerGeneratorInfo['name'],
+                                'config' => Json::decode($crackerGeneratorInfo['config'], true)
+                            ]);
+                        }
+                    }
+                }
+            } elseif ($reqData['vendor_type'] == 'generator') { // Generator's information
+                $info = \Yii::$app->db->createCommand("SELECT config FROM {{%generator}} WHERE name = :name", [
+                    ':name' => $reqData['name']
+                ])->queryScalar();
+                
+                if ($info === false)
+                    $info = '';
+                else
+                    $info = Json::decode($info);
+            }
+        }
+        
+        return $info;
     }
 
     /**
