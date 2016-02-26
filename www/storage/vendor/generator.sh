942997124197d24b3670745e9418ba5577b0b7d4
RezaSR <rsr_reza@yahoo.com>
2016-02-27 02:38:19 +0330

    Create markov generator too

--- www/storage/vendor/generator.sh
+++ www/storage/vendor/generator.sh
@@ -3,31 +3,47 @@
 PATH_ME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PATH_REPO="$PATH_ME/repo"
-PATH_BASE="$PATH_ME/generator"
+PATH_SERVER="$PATH_ME/generator"
 
-PATH_GENERATOR="$PATH_REPO/vendor-generator-general"
-if [ -d "$PATH_GENERATOR/.git" ]; then
-	cd "$PATH_GENERATOR"
-	git pull origin
-else
-	git clone https://github.com/d-bf/vendor-generator-general "$PATH_GENERATOR"
-	cd "$PATH_GENERATOR"
-fi
-
-make
-
-rm -R "$PATH_BASE/general/*"
-
-for os_arch in cpu_linux_64.bin cpu_linux_32.bin cpu_win_64.exe cpu_win_32.exe cpu_mac_64.app cpu_mac_32.app
+# Loop through vendors
+for vendor_name in general markov
 do
-	if [ -f "$PATH_GENERATOR/bin/$os_arch" ]; then
-		if [ ! -d "$PATH_BASE/general" ]; then
-			mkdir -p "$PATH_BASE/general"
-		fi
+	PATH_VENDOR_REPO="$PATH_REPO/vendor-generator-$vendor_name"
+	PATH_VENDOR_SERVER="$PATH_SERVER/$vendor_name"
 
-		zip -jr "$PATH_BASE/general/$os_arch.zip" "$PATH_GENERATOR/bin/$os_arch"
-		mv "$PATH_BASE/general/$os_arch.zip" "$PATH_BASE/general/${os_arch%.*}"
+	# Clone or update vendor
+	if [ -d "$PATH_VENDOR_REPO/.git" ]; then
+		cd "$PATH_VENDOR_REPO"
+		git pull origin
 	else
-		echo "Missing"
+		git clone "https://github.com/d-bf/vendor-generator-$vendor_name" "$PATH_VENDOR_REPO"
+		cd "$PATH_VENDOR_REPO"
 	fi
+
+	# Make vendor file(s) in repo
+	make
+
+	# Remove current vendor from server
+	rm -f -R "$PATH_VENDOR_SERVER/"
+
+	# Loop through os-arch
+	for os_arch in cpu_linux_64.bin cpu_linux_32.bin cpu_win_64.exe cpu_win_32.exe cpu_mac_64.app cpu_mac_32.app
+	do
+		# Check if the os-arch exists in repo
+		if [ -f "$PATH_VENDOR_REPO/bin/$os_arch" ]; then
+			mkdir -p "$PATH_VENDOR_SERVER"
+
+			# Compress vendor from repo to server
+			if [ "$vendor_name" == "markov" ]; then
+				zip -jr "$PATH_VENDOR_SERVER/$os_arch.zip" "$PATH_VENDOR_REPO/bin/$os_arch" "$PATH_VENDOR_REPO/dep-gen"
+			else
+				zip -jr "$PATH_VENDOR_SERVER/$os_arch.zip" "$PATH_VENDOR_REPO/bin/$os_arch"
+			fi
+
+			# Remove file extension
+			mv "$PATH_VENDOR_SERVER/$os_arch.zip" "$PATH_VENDOR_SERVER/${os_arch%.*}"
+		else
+			echo "Error, vendor not found: $PATH_VENDOR_REPO/bin/$os_arch"
+		fi
+	done
 done
