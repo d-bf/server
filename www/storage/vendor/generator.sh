6b48d583ea9b03daa37da320557d390ee60b57f0
RezaSR <rsr_reza@yahoo.com>
2016-02-27 01:26:13 +0330

    Preparing a script to automate vendor creation and synchronization

---
+++ www/storage/vendor/generator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PATH_REPO="./repo"
+PATH_BASE="./generator"
+
+PATH_GENERATOR="$PATH_REPO/vendor-generator-general"
+if [ -d "$PATH_GENERATOR/.git" ]; then
+	cd "$PATH_GENERATOR"
+	git pull origin
+else
+	git clone https://github.com/d-bf/vendor-generator-general "$PATH_GENERATOR"
+	cd "$PATH_GENERATOR"
+fi
+
+make
+
+rm -R $PATH_BASE/general/*
+
+for os_arch in cpu_linux_64.bin cpu_linux_32.bin cpu_win_64.exe cpu_win_32.exe cpu_mac_64.app cpu_mac_32.app
+do
+	if [ -f "$PATH_GENERATOR/bin/$os_arch" ]; then
+		if [ ! -d "$PATH_BASE/general" ]; then
+			mkdir -p "$PATH_BASE/general"
+		fi
+
+		zip -jr "$PATH_BASE/general/$os_arch.zip" "$PATH_GENERATOR/bin/$os_arch"
+		mv "$PATH_BASE/general/$os_arch.zip" "$PATH_BASE/general/${os_arch%.*}"
+	else
+		echo "Missing"
+	fi
+done
